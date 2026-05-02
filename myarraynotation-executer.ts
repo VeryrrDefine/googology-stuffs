95d28130c07c4aa31f454cd5d4fefd52e895c5b0
VeryrrDefine <1308061599@qq.com>
2026-05-02 13:28:57 +0800

    Update myarraynotation-executer

--- myarraynotation-executer.ts
+++ myarraynotation-executer.ts
@@ -1,6 +1,6 @@
 export type MANNumber = MyArrayNotation | number;
 class AdditionalTerm {
-  terms: number[] = [];
+  terms: MANNumber[] = [];
   clone() {
     let x = new AdditionalTerm();
     x.terms = Array.from(this.terms);
@@ -13,6 +13,29 @@ class AdditionalTerm {
   }
   toString() {
     return this.terms.join(",");
+  }
+  execute(x: MANNumber): AdditionalTerm {
+    if (this.terms[0] !== 1)
+      throw new Error("Cannot execute this additional term");
+    let newterm = Array.from(this.terms);
+    let last1 = 0;
+    for (let i = 0; i < newterm.length; i++) {
+      if (newterm[i] == 1) {
+        last1 = i;
+      } else {
+        break;
+      }
+    }
+    if (last1 == newterm.length - 1) throw new Error("Executed Term is all 1");
+    // {1,1,1...,1,2}
+    // {1,1,1...,10,1}
+
+    newterm[last1] = x;
+    newterm[last1 + 1] = substract(newterm[last1 + 1]);
+    let n = new AdditionalTerm();
+    n.terms = newterm;
+
+    return n;
   }
 }
 function substract(x: MANNumber) {
@@ -42,7 +65,7 @@ export class MyArrayNotation {
         if (typeof this.exponent !== "number") exp = this.exponent.calculate();
         else exp = this.exponent;
       } catch (e) {
-        throw new Error("Number to large");
+        throw new Error("Number to large or infinite-descending");
       }
       let calc = this.base ** exp;
       if (calc === Infinity) throw new Error("Number to large");
@@ -66,8 +89,15 @@ export class MyArrayNotation {
       outside.exponent = D(outside.exponent);
       return outside;
     }
-    let z = this.additionalTerm.terms[0] - 1;
-    if (z == 0) throw new Error("Not Implemented");
+    let z = substract(this.additionalTerm.terms[0]);
+    if (z == 0) {
+      let outside = new MyArrayNotation();
+      outside.base = this.base;
+      outside.exponent = this.base;
+      outside.additionalTerm = this.additionalTerm.execute(this.exponent);
+      return outside;
+      // throw new Error("Not Implemented");
+    }
     let Q = this.additionalTerm.terms.slice(1);
     let outside = new MyArrayNotation();
     outside.base = this.base;
@@ -75,7 +105,10 @@ export class MyArrayNotation {
     outside.exponent = new MyArrayNotation();
     outside.exponent.base = this.base;
     outside.exponent.exponent = substract(this.exponent);
-    outside.exponent.additionalTerm.terms = [z + 1, ...Q];
+    outside.exponent.additionalTerm.terms = [
+      this.additionalTerm.terms[0],
+      ...Q,
+    ];
     // console.log(`${this} = ${outside}`);
     return outside;
     // throw new Error("Not Implemented");
@@ -92,7 +125,7 @@ let man = new MyArrayNotation();
 man.base = 2;
 man.exponent = 2;
 man.additionalTerm = new AdditionalTerm();
-man.additionalTerm.terms = [1, 2];
+man.additionalTerm.terms = [2, 3, 5, 7, 9, 12];
 
 let man1: MyArrayNotation = man;
 let res: any = NaN;
